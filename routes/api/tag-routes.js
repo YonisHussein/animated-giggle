99484fe89148f9992a00647ceba3a4ddd3748563
YonisHussein <yonismhussein@gmail.com>
2022-07-31 07:55:27 -0400

    had to change repository

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(400).json({message: 'Tag was not found'})
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body,tag_name
+  })
+    .then(dbTagData => res,json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if(!dbTagData[0]) {
+      res.status(400).json({message: 'Tag was not found'})
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+ 
+})
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(400).json({message: 'Tag was not found'})
+        return;
+      }
+      res.json(dbTagData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    })
+})
+
+module.exports = router;
